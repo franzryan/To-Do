c5981d5f44a2b940b175e42f644723e90b4ac216
Franz Ryan <franz.ryan12@gmail.com>
2023-12-06 23:13:38 +0800

    fixed the save to local storage bug

--- modified.js
+++ modified.js
@@ -9,35 +9,74 @@ function saveToLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(taskArr))
 }
 
-function addTask() {
-    if (inputBox == '') {
+window.addEventListener('load', function(){
+    creationOfLi(true, taskArr)
+})
+
+function creationOfLi() {
+    taskArr.forEach(function(value) {
+                let createLi = document.createElement('li');
+                let createInput = document.createElement('input')
+                createInput.type = 'checkbox'
+                let label = document.createElement('label')
+                label.textContent = value
+                createLi.appendChild(createInput)
+                createLi.appendChild(label)
+                progressUl.appendChild(createLi)
+                createInput.addEventListener('change', function() {
+                    if (createInput.checked) {
+                        // Move the li element to the completed tasks container
+                        completedUl.appendChild(createLi);
+                    } else {
+                        // Move the li element back to the in-progress tasks container
+                        progressUl.appendChild(createLi);
+                    }
+                });
+            })
+}
+
+function addTask(fromPageLoad) {
+    if (inputBox.value === '' && !fromPageLoad) {
         alert('please enter a task')
     } else {
-    //add the input value to task array
-    taskArr.push(inputBox.value)
-    progressUl.innerHTML = '';
-    //iterate the task array and set an li element per index
-    taskArr.forEach(function(value) {
-        let createLi = document.createElement('li');
-        let createInput = document.createElement('input')
-        createInput.type = 'checkbox'
-        let label = document.createElement('label')
-        label.textContent = value
-        createLi.appendChild(createInput)
-        createLi.appendChild(label)
-        progressUl.appendChild(createLi)
-        createInput.addEventListener('change', function() {
-            if (createInput.checked) {
-                // Move the li element to the completed tasks container
-                completedUl.appendChild(createLi);
-            } else {
-                // Move the li element back to the in-progress tasks container
-                progressUl.appendChild(createLi);
-            }
-        });
-    })
-    //empty the input box
-    inputBox.value = ""
-    saveToLocalStorage()
-}}
+        //add the input value to task array
+        taskArr.push(inputBox.value)
+        progressUl.innerHTML = '';
+        creationOfLi()
+        inputBox.value = ""
+        saveToLocalStorage()
+}
+}
 
+// function addTask(fromPageLoad) {
+//     if (inputBox.value === '' && !fromPageLoad) {
+//         console.log("please enter a task")
+//         alert('please enter a task')
+//     } else {
+//     //add the input value to task array
+//     taskArr.push(inputBox.value)
+//     progressUl.innerHTML = '';
+//     //iterate the task array and set an li element per index
+//     taskArr.forEach(function(value) {
+//         let createLi = document.createElement('li');
+//         let createInput = document.createElement('input')
+//         createInput.type = 'checkbox'
+//         let label = document.createElement('label')
+//         label.textContent = value
+//         createLi.appendChild(createInput)
+//         createLi.appendChild(label)
+//         progressUl.appendChild(createLi)
+//         createInput.addEventListener('change', function() {
+//             if (createInput.checked) {
+//                 // Move the li element to the completed tasks container
+//                 completedUl.appendChild(createLi);
+//             } else {
+//                 // Move the li element back to the in-progress tasks container
+//                 progressUl.appendChild(createLi);
+//             }
+//         });
+//     })
+//     //empty the input box
+//     inputBox.value = ""
+//     saveToLocalStorage()
+// }}
