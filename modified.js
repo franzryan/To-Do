96581fbe2dcce383585a62916a63729edb94360c
Franz Ryan <franz.ryan12@gmail.com>
2023-12-11 01:17:20 +0800

    forgot to save the other files

--- modified.js
+++ modified.js
@@ -13,35 +13,35 @@ function saveToLocalStorage() {
 }
 
 window.addEventListener('load', function(){
-    creationOfLi(taskArr)
+    runProgressAndCompleted()
 })
 
-function creationOfLi() {
-    taskArr.forEach(function(value) {
-        console.log('creation ran')
+function creationOfLi(tasksArray, destinationUl) {
+    tasksArray.forEach(function(value) {
                 //create li element
                 let createLi = document.createElement('li');
+
                 //create input element for checkbox
                 let createInput = document.createElement('input')
                 createInput.type = 'checkbox'
+
                 //create label for the li element (cannot display text if without label and assign input to label content)
                 let label = document.createElement('label')
                 label.textContent = value
+
                 let deleteBtn = document.createElement('button')
                 deleteBtn.textContent = "Delete"
+
                 createLi.appendChild(createInput)
                 //append label
                 createLi.appendChild(label)
                 //append li to document
-                if (taskArr) {
-                    progressUl.appendChild(createLi)
-                    createLi.appendChild(deleteBtn)
-                    saveToLocalStorage()
-                } else if (completedTasks) {
-                    completedTasks.appendChild(createLi)
-                    createLi.appendChild(deleteBtn)
-                    saveToLocalStorage()
-                }
+                destinationUl.appendChild(createLi)
+                createLi.appendChild(deleteBtn)
+                saveToLocalStorage()
+                    if (destinationUl === completedUl) {
+                    createInput.checked = true
+                    }
                 //checkbox eventlistener
                 createInput.addEventListener('change', function() {
                     if (createInput.checked) {
@@ -61,12 +61,10 @@ function creationOfLi() {
                     }
                 });
                 deleteBtn.addEventListener('click', () => {
-                    console.log(label.textContent)
-                    taskArr.forEach(function(item) {
+                    tasksArray.forEach(function(item) {
                         if (label.textContent == item) {
-                            let itemIndex = taskArr.indexOf(item)
-                            taskArr.splice(itemIndex, 1)
-                            console.log(taskArr)
+                            let itemIndex = tasksArray.indexOf(item)
+                            tasksArray.splice(itemIndex, 1)
                             createLi.remove()
                             saveToLocalStorage()
                         }
@@ -76,18 +74,29 @@ function creationOfLi() {
             saveToLocalStorage()
 }
 
-function addTask(fromPageLoad) {
-    if (inputBox.value === '' && !fromPageLoad) {
+function addTask() {
+    if (inputBox.value === '') {
         alert('please enter a task')
     } else {
         //add the input value to task array
         taskArr.push(inputBox.value)
-        // progressUl.innerHTML = '';
-        creationOfLi()
-        document.
+        progressUl.textContent = ""
+        completedUl.textContent = ""
+        runProgressAndCompleted()
         inputBox.value = ""
-        progressUl.innerHTML = ''
-        completedUl.innerHTML = ''
         saveToLocalStorage()
 }
+}
+
+function runProgressAndCompleted() {
+    creationOfLi(taskArr, progressUl);
+    creationOfLi(completedTasks, completedUl)
+}
+
+function deleteAll() {
+    taskArr = []
+    completedTasks = []
+    progressUl.textContent = ''
+    completedUl.textContent = ''
+    saveToLocalStorage()
 }
