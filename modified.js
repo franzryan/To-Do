976c63e1b7cc041872895879a0a1d9084f553362
Franz Ryan <franz.ryan12@gmail.com>
2023-12-11 00:19:43 +0800

    fully functional to do list app

--- modified.js
+++ modified.js
@@ -3,10 +3,13 @@ let progressUl = document.getElementById('inprogress-tasks')
 let completedUl = document.getElementById('completed-tasks')
 
 
-let taskArr = JSON.parse(localStorage.getItem('tasks')) || []
+let taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
+let completedTasks = JSON.parse(localStorage.getItem('done')) || [];
+
 
 function saveToLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(taskArr))
+    localStorage.setItem('done', JSON.stringify(completedTasks))
 }
 
 window.addEventListener('load', function(){
@@ -29,20 +32,33 @@ function creationOfLi() {
                 createLi.appendChild(createInput)
                 //append label
                 createLi.appendChild(label)
-                //append li 
-                progressUl.appendChild(createLi)
-                createLi.appendChild(deleteBtn)
+                //append li to document
+                if (taskArr) {
+                    progressUl.appendChild(createLi)
+                    createLi.appendChild(deleteBtn)
+                    saveToLocalStorage()
+                } else if (completedTasks) {
+                    completedTasks.appendChild(createLi)
+                    createLi.appendChild(deleteBtn)
+                    saveToLocalStorage()
+                }
+                //checkbox eventlistener
                 createInput.addEventListener('change', function() {
                     if (createInput.checked) {
                         // Move the li element to the completed tasks container
+                        completedTasks.push(label.textContent)
                         completedUl.appendChild(createLi);
+                        let indexOfRemovingItem = taskArr.indexOf(label.textContent)
+                        taskArr.splice(indexOfRemovingItem, 1)
                         saveToLocalStorage()
                     } else {
                         // Move the li element back to the in-progress tasks container
+                        taskArr.push(label.textContent)
                         progressUl.appendChild(createLi);
+                        let indexOfRemovingItem = completedTasks.indexOf(label.textContent)
+                        completedTasks.splice(indexOfRemovingItem, 1)
                         saveToLocalStorage()
                     }
-                    saveToLocalStorage()
                 });
                 deleteBtn.addEventListener('click', () => {
                     console.log(label.textContent)
@@ -66,15 +82,12 @@ function addTask(fromPageLoad) {
     } else {
         //add the input value to task array
         taskArr.push(inputBox.value)
-        progressUl.innerHTML = '';
+        // progressUl.innerHTML = '';
         creationOfLi()
+        document.
         inputBox.value = ""
+        progressUl.innerHTML = ''
+        completedUl.innerHTML = ''
         saveToLocalStorage()
 }
-}
-
-function deleteAllTask() {
-    taskArr = []
-    console.log('hello')
-    creationOfLi(taskArr)
 }
